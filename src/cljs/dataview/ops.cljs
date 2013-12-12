591b5681dcbd435484643d57b12a24255ef26f24
Richard Hull <rm_hull@yahoo.co.uk>
2013-12-12 00:49:55 +0000

    Function documentation

--- src/cljs/dataview/ops.cljs
+++ src/cljs/dataview/ops.cljs
@@ -2,17 +2,44 @@
 
 ; TODO - turn into protocol & extend DataView
 
-(defn get-uint8 [data-view byte-offset]
+(defn get-uint8
+  "Gets the value of the 8-bit unsigned byte at the specified byte offset
+   from the start of the data view. There is no alignment constraint;
+   multi-byte values may be fetched from any offset."
+  [data-view byte-offset]
   (. data-view (getUint8 byte-offset)))
 
-(defn get-uint32-le [data-view byte-offset]
+(defn get-uint16-le
+  "Gets the value of the 16-bit little-endian unsigned short integer at the
+   specified byte offset from the start of the data view. There is no
+   alignment constraint; multi-byte values may be fetched from any offset."
+  [data-view byte-offset]
+  (. data-view (getUint16 byte-offset true)))
+
+(defn get-uint32-le
+  "Gets the value of the 32-bit little-endian unsigned integer at the
+   specified byte offset from the start of the data view. There is no
+   alignment constraint; multi-byte values may be fetched from any offset."
+  [data-view byte-offset]
   (. data-view (getUint32 byte-offset true)))
 
-(defn get-float32-le [data-view byte-offset]
+(defn get-float32-le
+  "Gets the value of the 32-bit little-endian IEEE floating-point number
+   at the specified byte offset from the start of the data view. There is no
+   alignment constraint; multi-byte values may be fetched from any offset."
+  [data-view byte-offset]
   (. data-view (getFloat32 byte-offset true)))
 
-(defn get-string [data-view byte-offset byte-length & [encoding]]
-  (->>
-    (range byte-offset (+ byte-offset byte-length))
-    (map (comp char (partial get-uint8 data-view)))
-    (apply str)))
+(def get-string
+  "Given a data-view, a byte offset, a length (and optionally an encoding -
+   note that only UTF-8 is currently supported), extracts a string from the
+   underlying byte buffer."
+  (let [utf? #{:utf8 :utf-8 :UTF8 :UTF-8}]
+    (fn [data-view byte-offset byte-length & [encoding]]
+      (let [s (->>
+                (range byte-offset (+ byte-offset byte-length))
+                (map (comp char (partial get-uint8 data-view)))
+                (apply str))]
+        (if (utf? (keyword encoding))
+          (-> s js/escape js/decodeURIComponent)
+          s)))))
