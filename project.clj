f046b5bc9c79c92da687bc0de818a5b39c59eca0
Richard Hull <rm_hull@yahoo.co.uk>
2014-01-12 17:24:22 +0000

    Use own fork of cljs-test

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :global-vars {*warn-on-reflection* true}
   :plugins [[lein-cljsbuild "1.0.0"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [prismatic/cljs-test "0.0.6"]
+                 [rm-hull/cljs-test "0.0.7"]
                  [org.clojure/clojurescript "0.0-2127"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
   :cljsbuild {
