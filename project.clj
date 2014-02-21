885f81f9580bb32f9d6676c26208474cabcbc9e1
Richard Hull <rm_hull@yahoo.co.uk>
2014-02-21 00:36:54 +0000

    Directory restructure

--- project.clj
+++ project.clj
@@ -1,31 +1,36 @@
 (defproject rm-hull/cljs-dataview "0.0.1-SNAPSHOT"
   :clojurescript? true
   :description "A ClojureScript library for asynchronously fetching & dicing remote binary objects"
-  :source-paths ["src/cljs"]
+  :source-paths ["src"]
   :url "https://github.com/rm-hull/cljs-dataview"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :scm {:url "git@github.com:rm-hull/cljs-dataview"}
   :min-lein-version "2.3.4"
   :global-vars {*warn-on-reflection* true}
-  :plugins [[lein-cljsbuild "1.0.0"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [rm-hull/cljs-test "0.0.7"]
-                 [org.clojure/clojurescript "0.0-2138"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [rm-hull/cljs-test "0.0.7"]]
   :cljsbuild {
     :test-commands {"phantomjs" ["phantomjs" "target/unit-test.js"]}
     :builds {
-      :dev {:source-paths ["src" "example"]
-            :incremental? true
-            :compiler {:output-to "target/example.js"
-                       :optimizations :whitespace
-                       :pretty-print true
-                       ;:source-map true
-                       }}
-      :test {:source-paths ["src" "test"]
-             :incremental? true
-             :compiler {:output-to "target/unit-test.js"
-                        :optimizations :whitespace
-                        :pretty-print true }}}
-              })
+      :dev {
+        :source-paths ["src" "example"]
+        :incremental? true
+        :compiler {
+          :output-to "target/example.js"
+          :source-map "target/example.map"
+          :static-fns true
+          :optimizations :whitespace
+          :pretty-print true }}
+      :test {
+        :source-paths ["src" "test"]
+        :incremental? true
+        :compiler {
+          :output-to "target/unit-test.js"
+          :source-map "target/unit-test.map"
+          :static-fns true
+          :optimizations :whitespace
+          :pretty-print true }}}})
