0e60c5f0c4d66e28ee9a180d2aba71c91e899819
Richard Hull <rm_hull@yahoo.co.uk>
2014-02-24 22:01:25 +0000

    Updated clojurescript version

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :global-vars {*warn-on-reflection* true}
   :plugins [[lein-cljsbuild "1.0.2"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [rm-hull/cljs-test "0.0.7"]]
   :cljsbuild {
