b0b438411f8f9f30fd4f225054085487a69ce4f7
Richard Hull <rm_hull@yahoo.co.uk>
2014-02-24 22:09:33 +0000

    Fix example

--- example/binary_stl.cljs
+++ example/binary_stl.cljs
@@ -1,43 +1,43 @@
 (ns binary-stl
-  (:require [dataview.loader :refer [fetch-blob]]
-            [dataview.ops :refer [create-reader read-fixed-string read-float32-le
-                                  read-uint16-le read-uint32-le]])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+  (:require
+    [dataview.loader :refer [fetch-blob]]
+    [dataview.ops :refer [create-reader]]
+    [dataview.protocols :as proto])
+  (:require-macros
+    [cljs.core.async.macros :refer [go]]))
 
-(set! *print-fn*
-  (fn [s]
-    (.log js/console s)))
+(enable-console-print!)
 
 ; Note: CORS -- http://www.w3.org/wiki/CORS#XMLHttpRequest_.28XHR.29
 ; Github slings back a 301, and corsproxy just forwards that so the
 ; remote-url doesnt quite work a.t.m.
-(def remote-url "http://www.corsproxy.com/github.com/rm-hull/wireframes/raw/master/doc/gallery/torus.stl")
+(def remote-url "http://programming-enchiladas.destructuring-bind.org/proxy?url=https://raw.github.com/rm-hull/wireframes/master/doc/gallery/torus.stl")
 (def local-url "http://localhost/~rhu/test/torus.stl")
 
 ;; Notice how close (in structure) this is to the similar gloss definition:
 ;; https://github.com/rm-hull/wireframes/blob/master/src/wireframes/shapes/stl_loader.clj#L11
 (defn point-spec [reader]
   (array-map
-    :x (read-float32-le reader)
-    :y (read-float32-le reader)
-    :z (read-float32-le reader)))
+    :x (proto/read-float32-le reader)
+    :y (proto/read-float32-le reader)
+    :z (proto/read-float32-le reader)))
 
 (defn triangle-spec [reader]
   (array-map
     :normal (point-spec reader)
     :points (doall (repeatedly 3 #(point-spec reader)))
-    :attributes (read-uint16-le reader)))
+    :attributes (proto/read-uint16-le reader)))
 
 (defn stl-spec [reader]
   (array-map
-    :header (read-fixed-string reader 80)
+    :header (proto/read-fixed-string reader 80)
     :triangles (doall (repeatedly
-                 (read-uint32-le reader)
+                 (proto/read-uint32-le reader)
                  #(triangle-spec reader)))))
 
 (go
   (->
-    (<! (fetch-blob local-url))
+    (<! (fetch-blob remote-url))
     (create-reader)
     (stl-spec)
     (time)
